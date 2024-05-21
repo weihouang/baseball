c533d0819079fb4a88e0cf036ac66a4217de885b
weihouang <willy200335@gmail.com>
2024-05-21 19:39:40 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <ChakraProvider>
+      <div>App</div>
+    </ChakraProvider>
+  );
+}
